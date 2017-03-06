98aaa7a01e40cbceeae811133685d3f27a7caa8d
lkm <rksym343@naver.com>
2017-03-06 11:01:53 +0900

    added output Tell his World

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
 	public static void main(String[] args){
 	System.out.println("hello java");
 	System.out.println("Tell your World");
+	System.out.println("Tell his World");
 	}
 }
