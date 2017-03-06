a2b476e33ad5d359fe80c16a6a08a08b9b5144a8
lkm <rksym343@naver.com>
2017-03-06 11:08:10 +0900

    added Tell his world

--- Hello.java
+++ Hello.java
@@ -2,5 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 	System.out.println("hello java");
 	System.out.println("Tell your World");
+	System.out.println("Tell her World");
 	}
+
 }
